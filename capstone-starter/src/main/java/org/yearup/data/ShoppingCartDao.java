3e13d8b97d617317ab4110ea111df7a520e1e7a0
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-06-24 10:00:05 -0700

    shopping cart phase

--- capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,19 @@
 package org.yearup.data;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+
+import java.security.Principal;
 
 public interface ShoppingCartDao
 {
-    ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+    ShoppingCart getCartByUserId(int userId);
+
+    void addToCart(int userId, int productId);
+
+    void updateCartItem(int userId, int productId, int quantity);
+
+    void clearCart(int userId);
 }
--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -8,22 +8,19 @@ import javax.sql.DataSource;
 import java.sql.*;
 
 @Component
-public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
-{
-    public MySqlProfileDao(DataSource dataSource)
-    {
+public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao {
+
+    public MySqlProfileDao(DataSource dataSource) {
         super(dataSource);
     }
 
     @Override
-    public Profile create(Profile profile)
-    {
+    public Profile create(Profile profile) {
         String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
-                " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        try(Connection connection = getConnection())
-        {
-            PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+        try (Connection connection = getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(sql);
             ps.setInt(1, profile.getUserId());
             ps.setString(2, profile.getFirstName());
             ps.setString(3, profile.getLastName());
@@ -35,13 +32,66 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
             ps.setString(9, profile.getZip());
 
             ps.executeUpdate();
-
             return profile;
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+    @Override
+    public Profile getByUserId(int userId) {
+        String sql = "SELECT * FROM profiles WHERE user_id = ?";
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, userId);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return mapRow(rs);
+            }
+
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void update(Profile profile) {
+        String sql = "UPDATE profiles SET first_name = ?, last_name = ?, phone = ?, email = ?, address = ?, city = ?, state = ?, zip = ? " +
+                "WHERE user_id = ?";
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, profile.getFirstName());
+            ps.setString(2, profile.getLastName());
+            ps.setString(3, profile.getPhone());
+            ps.setString(4, profile.getEmail());
+            ps.setString(5, profile.getAddress());
+            ps.setString(6, profile.getCity());
+            ps.setString(7, profile.getState());
+            ps.setString(8, profile.getZip());
+            ps.setInt(9, profile.getUserId());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Profile mapRow(ResultSet rs) throws SQLException {
+        Profile profile = new Profile();
+        profile.setUserId(rs.getInt("user_id"));
+        profile.setFirstName(rs.getString("first_name"));
+        profile.setLastName(rs.getString("last_name"));
+        profile.setPhone(rs.getString("phone"));
+        profile.setEmail(rs.getString("email"));
+        profile.setAddress(rs.getString("address"));
+        profile.setCity(rs.getString("city"));
+        profile.setState(rs.getString("state"));
+        profile.setZip(rs.getString("zip"));
+        return profile;
+    }
 }
